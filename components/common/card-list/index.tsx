74d5aab86c8813684fb48bb7766435ebd60d7a63
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2022-09-27 13:19:18 -0300

    feat: Responsive Design

--- components/common/card-list/index.tsx
+++ components/common/card-list/index.tsx
@@ -2,14 +2,19 @@ import ICardList from "../../../interfaces/ICardList";
 import Card from "../card";
 
 export default function CardList(props: ICardList) {
+  const { arrCharacters } = props;
 
-    const { arrCharacters } = props;
-
-    return (
-        <div className="flex flex-row flex-wrap justify-between gap-4">
-            {arrCharacters?.map(character =>
-                <Card key={character.id} id={character.id} image={character.image} name={character.name} species={character.species} />
-            )}
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
+      {arrCharacters?.map((character) => (
+        <Card
+          key={character.id}
+          id={character.id}
+          image={character.image}
+          name={character.name}
+          species={character.species}
+        />
+      ))}
+    </div>
+  );
 }
--- components/section/index.tsx
+++ components/section/index.tsx
@@ -1,24 +1,17 @@
 interface ISection {
-    title: string;
-    description: string;
-    content: JSX.Element;
+  title: string;
+  description: string;
+  content: JSX.Element;
 }
 
 export default function Section(props: ISection) {
+  const { title, description, content } = props;
 
-    const { title, description, content } = props;
-
-    return (
-        <section className="h-full text-white bg-swgunmetal text-swbone p-32">
-            <h1 className="text-3xl mb-4">
-                {title}
-            </h1>
-            <p className="text-lg mb-4">
-                {description}
-            </p>
-            <div>
-                {content}
-            </div>
-        </section>
-    )
+  return (
+    <section className="h-full text-white bg-swgunmetal text-swbone p-4 lg:p-32">
+      <h1 className="text-3xl mb-4">{title}</h1>
+      <p className="text-lg mb-4">{description}</p>
+      <div>{content}</div>
+    </section>
+  );
 }
