12e43927702875394ff37638c3f7ebd099df3338
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-10-15 13:30:14 -0300

    bug: ok

--- components/common/card-list/index.tsx
+++ components/common/card-list/index.tsx
@@ -4,30 +4,11 @@ import Card from "../card";
 export default function CardList(props: ICardList) {
   const { arrCharacters } = props;
 
-<<<<<<< HEAD
-    const { arrCharacters } = props;
-
-    return (
-        <div className="flex flex-col md:flex-wrap md:flex-row lg:flex-wrap lg:flex-row gap-4">
-            {arrCharacters?.map(character =>
-                <Card key={character.id} id={character.id} image={character.image} name={character.name} species={character.species} />
-            )}
-        </div>
-    )
-}
-=======
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-      {arrCharacters?.map((character) => (
-        <Card
-          key={character.id}
-          id={character.id}
-          image={character.image}
-          name={character.name}
-          species={character.species}
-        />
-      ))}
+    <div className="flex flex-col md:flex-wrap md:flex-row lg:flex-wrap lg:flex-row gap-4">
+      {arrCharacters?.map(character =>
+        <Card key={character.id} id={character.id} image={character.image} name={character.name} species={character.species} />
+      )}
     </div>
-  );
+  )
 }
->>>>>>> 74d5aab86c8813684fb48bb7766435ebd60d7a63
